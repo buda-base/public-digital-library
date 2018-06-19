31c9688ee892d947dc7db534ffcab2642de957d5
berger-n <quai.ledrurollin@gmail.com>
2018-06-19 17:06:32 +0200

    removed chunk numbers in etexts

--- src/state/sagas/index.js
+++ src/state/sagas/index.js
@@ -189,7 +189,7 @@ async function getChunks(iri,next) {
 
       data = _.sortBy(data["@graph"],'seqNum')
       .filter(e => e.chunkContents)
-      .map(e => ({ value:e.chunkContents["@value"]+ " ("+e.seqNum+")" }))
+      .map(e => ({ value:e.chunkContents["@value"] })); //+ " ("+e.seqNum+")" }))
 
       console.log("dataC",iri,next,data)
 
