94e6232c458e6cd66573eb5dda41b8dc2f98fe92
berger-n <quai.ledrurollin@gmail.com>
2018-07-25 19:09:50 +0200

    added automatic token renewal 5min before expiration

--- src/Auth.js
+++ src/Auth.js
@@ -2,6 +2,49 @@ import auth0 from 'auth0-js';
 import history from "./history"
 import store from "./index"
 import * as ui from "./state/ui/actions"
+import {auth} from "./routes"
+
+var tokenRenewalTimeout;
+
+function scheduleRenewal() {
+  var expiresAt = JSON.parse(localStorage.getItem('expires_at')) - 5*60*1000 ;
+  var delay = expiresAt - Date.now();
+  console.log("delay",delay)
+  if (delay > 0) {
+    tokenRenewalTimeout = setTimeout(function() {
+      renewToken();
+    }, delay);
+  }
+}
+
+function renewToken() {
+  auth.auth1.checkSession({},
+    function(err, result) {
+      if (err) {
+        console.log("renew token error",err);
+      } else {
+      console.log("renew token ok!",result)
+        setSession(result);
+      }
+    }
+  );
+}
+
+function setSession(authResult) {
+  // Set the time that the Access Token will expire at
+  var expiresAt = JSON.stringify(
+    authResult.expiresIn * 1000 + new Date().getTime()
+  );
+  localStorage.setItem('access_token', authResult.accessToken);
+  localStorage.setItem('id_token', authResult.idToken);
+  localStorage.setItem('expires_at', expiresAt);
+  scheduleRenewal();
+}
+
+window.addEventListener('load', function() {
+  // ...
+  scheduleRenewal();
+});
 
 
 export default class Auth {
@@ -58,6 +101,7 @@ export default class Auth {
     // navigate to the home route
     history.replace('/');
     store.dispatch(ui.logEvent(false))
+    clearTimeout(tokenRenewalTimeout);
   }
 
   isAuthenticated() {
@@ -66,6 +110,9 @@ export default class Auth {
     let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
     return new Date().getTime() < expiresAt;
   }
+
+
+
 }
 
 
