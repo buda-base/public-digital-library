3f36fb53a8a5d97f95307823284d0693d9370ac5
berger-n <quai.ledrurollin@gmail.com>
2018-08-07 18:50:11 +0200

    added automated json-server launch from js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { shallow } from 'enzyme';
 import store from '../index';
 import {initiateApp} from '../state/actions';
+import tcpPortUsed from 'tcp-port-used'
 
 
 require('../setupTests')
@@ -12,31 +13,31 @@ let bdrcAPI = require('../lib/api').default;
 
 describe('main App tests', () => {
 
+   it('loading config file from json-server', async() => {
+      tcpPortUsed.waitUntilUsed(5555).then(async() => {
 
-   it('load json-server config file', async() => {
+         const api = new bdrcAPI({server:"http://localhost:5555"});
 
-      const api = new bdrcAPI({server:"http://localhost:5555"});
+         let config =  await api.getURLContents("http://localhost:5555/config.json",false);
+         expect(config).toMatchSnapshot()
 
-      let config =  await api.getURLContents("http://localhost:5555/config.json",false);
-      expect(config).toMatchSnapshot()
+         config =  await api.loadConfig();
+         expect(config).toMatchSnapshot()
 
-      config =  await api.loadConfig();
-      expect(config).toMatchSnapshot()
-
+      })
    })
 
-   it('App initialization test', async done => {
+   it('App initialization', async done => {
+      tcpPortUsed.waitUntilUsed(5555).then(async () => {
 
-      const div = document.createElement('div');
-      let compo ;
-      ReactDOM.render( compo = makeRoutes(), div);
-      expect(compo).toMatchSnapshot()
+         const div = document.createElement('div');
+         let compo ;
+         ReactDOM.render( compo = makeRoutes(), div);
+         expect(compo).toMatchSnapshot()
 
-      // fix to avoid jest error (inspired from https://stackoverflow.com/questions/51226539/formik-testing-error-the-document-global-was-defined-when-react-was-initializ)
-      setTimeout( () => { done(); }, 200 ); // (100ms is too short on this computer but 200 is ok)
+         // inspired from https://stackoverflow.com/questions/51226539/formik-testing-error-the-document-global-was-defined-when-react-was-initializ
+         setTimeout( () => { done(); }, 200 ); // (100ms is too short on this computer but 200 is ok)
+      })
    });
-
-
-
 
 })
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,9 @@
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
+import jsonServer from 'json-server'
+import tcpPortUsed from 'tcp-port-used'
+
 import { XMLHttpRequest } from 'xmlhttprequest';
 global.XMLHttpRequest = XMLHttpRequest;
 
@@ -41,3 +44,22 @@ Object.defineProperty(window, 'fetch', { value: function(url,options,param)
    }
 });
 */
+
+tcpPortUsed.check(5555).then((inUse) => {
+
+   //console.log("checked port",inUse)
+
+   if(!inUse)
+   {
+
+      const server = jsonServer.create()
+      const router = jsonServer.router('src/tests/data.json')
+      const middlewares = jsonServer.defaults()
+
+      server.use(middlewares)
+      server.use(router)
+      server.listen(5555, () => {
+         console.log('JSON Server is running')
+      })
+   }
+})
