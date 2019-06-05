e212e166092146b944a13094a90a3659ddd88343
berger-n <quai.ledrurollin@gmail.com>
2019-06-05 18:16:46 +0200

    added download button for etexts + robots.txt file

--- src/components/ResourceViewer.js
+++ src/components/ResourceViewer.js
@@ -252,6 +252,11 @@ function top_left_menu(that,pdfLink,monoVol,fairUse)
        { /*  TODO // external resources ==> /query/graph/ResInfo?R_RES=
           that.props.IRI.match(/^bda[cn]:/) &&
        */}
+       { that.props.IRI && getEntiType(that.props.IRI) === "Etext" && <a target="_blank" style={{fontSize:"26px"}} download className="goBack pdfLoader" href={that.props.IRI?that.props.IRI.replace(/bdr:/,bdr)+".txt":""}>
+                <IconButton title={I18n.t("resource.download")+" TXT"}>
+                   <img src="/DL_icon.svg" height="24" />
+                </IconButton>
+               </a> }
        {pdfLink && 
          ( (!(that.props.manifestError && that.props.manifestError.error.message.match(/Restricted access/)) && !fairUse) ||
          (that.props.auth && that.props.auth.isAuthenticated()))
