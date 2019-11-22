faa7fcaab83837ee62700e6ce429b889fd743eef
berger-n <quai.ledrurollin@gmail.com>
2019-11-22 17:39:18 +0100

    fix volumeNumber display

--- src/components/ResourceViewer.js
+++ src/components/ResourceViewer.js
@@ -1145,7 +1145,7 @@ class ResourceViewer extends Component<Props,State>
                if(!lang) lang = infoBase[0]["lang"]
                if(lang) info = infoBase[0].value 
                else info = null
-               if(infoBase[0].type && infoBase[0].type == bdo+"volumeNumber") info = "Volume "+info ;
+               if(infoBase[0].type && infoBase[0].type == bdo+"volumeNumber") info = "Volume "+infoBase[0].value ;
                else if(info && info.match(/purl[.]bdrc/)) info = null
                //console.log("info0",info)
             }
