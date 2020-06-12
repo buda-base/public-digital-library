124add6ee4d5c26dc6836df92d92cc3189c5eaea
ngawangtrinley <ngawangtrinley@gmail.com>
2020-06-12 14:46:21 +0800

    Update bo.js

--- src/translations/bo.js
+++ src/translations/bo.js
@@ -1,7 +1,156 @@
 
-const boTranslation = {
+const enTranslation = {
    punc: {
-      colon:" "
+      colon:":"
+   },
+   home:{
+      BUDA:"BUDA",
+      titleBDRC1:"Buddhist",
+      titleBDRC2:"Digital Resource",
+      titleBDRC3:"Center",
+      archives1:"The Buddhist",
+      archives2:"Digital Archives",
+      by:"by the Buddhist Digital Resource Center",
+      subtitle:"A cooperative platform for expanding access to Buddhist literature. Explore the millions of pages of texts contributed by BDRC and its many partners.",
+      message:"WELCOME TO THE BETA VERSION OF BDRC's NEW LIBRARY!",
+      submessage:"with new features, resources, and tools for faster and more in-depth research",
+      search:"Search",
+      choose:"Choose language"
+   },
+   types:{
+      any:"Any",
+      corporation:"Corporation",
+      corporation_plural:"Corporations",
+      etext:"in Etext content",
+      etext_plural:"in Etext content",
+      etext_title:"Etext",
+      item:"Item",
+      item_plural:"Items",
+      lineage:"ཆོས་བརྒྱུད།",
+      lineage_plural:"ཆོས་བརྒྱུད།",
+      person:"མི་སྣ།",
+      person_plural:"མི་སྣ།",
+      place:"ས་གནས།",
+      place_plural:"ས་གནས།",
+      role:"ལས་འགན།",
+      role_plural:"ལས་འགན།",
+      topic:"བརྗོད་གཞི།",
+      topic_plural:"བརྗོད་གཞི།",
+      work:"བརྩམས་ཆོས།",
+      work_plural:"བརྩམས་ཆོས།",
+      instance:"དཔེ་ཆ།",
+      instance_plural:"དཔེ་ཆ།",
+      images:"བཤེར་པར།",
+      volume:"བཤེར་པར།"
+   },
+   topbar:{
+      about:"about us",
+      search:"new search",
+      login:"log in",
+      register:"register",
+      logout:"log out",
+      profile:"profile",
+      donate:"donate",
+      closeEtext:"Close Etext",
+      history:"Search history",
+      bookmarks:"Bookmarks",
+      results:"Search results for",
+      instances:"instances of"
+   },
+   Lsidebar:{
+      sortBy:{
+         title:"sort by"
+      },
+      title:"ནང་གསེས་འདེམས་སྒྲུག",
+      collection:{
+         title:"Collection"
+      },
+      datatypes:{
+         title:"དན་གྲངས་ཀྱི་རིགས།"
+      },
+      widgets:{
+         root: "In Instance",
+         tree: "Genre / Topic",
+         relationInv: "Inverse Relation Type"
+      },
+      tags:{
+         type: "Type",
+         instanceOf: "Instance Of",
+         reset:"Reset filters",
+         include:"Include results with",
+         exclude:"Exclude results with"
+
+      },
+      activeF:{
+         title:"Active Filters",
+         remove:"Remove filter",
+         reset:"Clear all filters"
+      }
+   },
+   sort:{
+      reverse:"Reverse Order",
+      popu:"Popularity",
+      closestM:"Closest Matches", 
+      workT:"Work Title",
+      personN:"Person Name", 
+      yearB:"Year of Birth",
+      placeN:"Place Name",
+      yearP:"Year of Publication",
+      title:"Title",
+      numberMC:"Number of Matching Chunks"
+   },
+   copyright:{
+      open:"Open Access",
+      fairUse:"Fair Use - Access to 20 first/last pages",
+      tempo:"Temporarily Restricted",
+      sealed:"Sealed",
+      view:"View Images",
+      scans:"Scans Available"
+   },
+   prop:{
+      "tmp:bestMatch":"Best Match",
+      "tmp:relationType":"Relation Type",
+      "tmp:nameMatch":"Name Match",
+      "tmp:nameMatch_plural":"Name Matches",
+      "tmp:otherTitle":"Other Title",
+      "tmp:otherTitle_plural":"Other Titles",
+      "tmp:otherName":"Other Name",
+      "tmp:otherName_plural":"Other Names",
+      "tmp:otherMatch":"Other Match ({{count}})",
+      "tmp:otherMatch_plural":"Other Matches ({{count}})",
+      "tmp:originalRecord":"Original Record",
+      "tmp:imageVolumeId":"Image Volume Id",
+      "tmp:workHasTranslationInCanonicalLanguage":"Canonical Translation",
+      "tmp:workHasTranslationInNonCanonicalLanguage":"Other Translation",
+      "tmp:entityScore":"Entity Score",
+      "tmp:instanceLabel":"Instance Label",
+      "tmp:dimensions":"Dimensions",
+      "tmp:publisherLocation":"Publisher Loc."
+   },
+   result:{
+      open:"Open",
+      resource:"resource",
+      in:"in",
+      available:"Resource available at",
+      inRootInstance:"In",      
+      workBy:"By",
+      eTextIsForWork:"For Work",
+      year:"Year",
+      inInstance:"In Instance",
+      inInstancePart:"In Instance Part",
+      expandC:"Expand Context",
+      hideC:"Hide Context",
+      openE:"Open Etext",
+      hasInstance:"Has {{count}} Instance",
+      hasInstance_plural:"Has {{count}} Instances",
+      hasInstance_plural_bo_example:"Has {{count,counttobo}} Instances"
+   },
+   index:{
+      openViewer:"Open in Viewer",
+      mainInfo:"Main Information",
+      outline:"Outline",
+      related:"Related Works",
+      extended:"Extended Information"
    },
    user:{
       photo:{ 
@@ -18,16 +167,45 @@ const boTranslation = {
          show:"Set public",
       }
    },
+   popover:{
+      moreInfo:"More Information",
+      source:"Source",
+      source_plural:"Sources",
+      notes:"Notes",
+      discussion:"Discussion",
+      lang:"Language/Script",
+      otherLang:"In Other Language",
+      otherLang_plural:"In Other Languages",
+      calendar:"In Calendar",
+      gregorian:"Gregorian Calendar",
+      imported:"This record was imported from",
+      seeO:"See original"
+   },
    misc:{
+      colon:":",
       datatype:"Back to datatypes",
+      has:"Has",
+      or:"or",
+      seeA:"see all",
+      browse:"browse all",
       show:"འདི་ལས་མང་བ།",
-      hide:"Hide"
+      seeMore:"See More",
+      readM:"Read more",
+      hide:"Hide",
+      preview:"preview",
+      permalink:"Permalink",
+      clipboard:"Url has been copied to clipboard.\nCTRL+V to paste",
+      from:"from",
+      in:"in",
+      of:"of",
+      please:"Please"
    },
    lang:{
       en:"English",
       fr:"French",
       zh:"Chinese",
       bo:"Tibetan",
+      km:"Khmer",
       lg:"Language",
       search:{
          zh:"Chinese",
@@ -46,6 +224,7 @@ const boTranslation = {
          boXDts:"Tibetan (DTS)",
          boAlaLc:"Tibetan (ALA-LC)",
          other:"Other",
+         km:"Khmer",
          inc:"Indic",
          sa:"Sanskrit",
          hans:"Simplified",
@@ -63,8 +242,11 @@ const boTranslation = {
          zh:"Chinese",
          zhHant:"Chinese (Hanzi)",
          //zhHans:"Chinese (Simplified)",
+         zhHant:"Chinese in traditional characters",
+         zhHans:"Chinese in simplified characters",
          zhLatnPinyin:"Chinese in Pinyin",
          en:"English",
+         km:"Khmer",
          saXIast:"Sanskrit transliterated in IAST",
          saDeva:"Sanskrit in Devanagari",
          bo:"Tibetan in Unicode",
@@ -76,6 +258,7 @@ const boTranslation = {
          pi:"པཱ་ལི།",
          bo:"བོད་ཡིག",
          inc:"རྒྱ་གར་ཡུལ་གྱི་སྐད་རིགས།",
+         km:"Khmer",
          sa:"ལེགས་སྦྱར།",
          hans:"གསར་མ།",
          hant:"སྲོལ་རྒྱུན།",
@@ -90,39 +273,12 @@ const boTranslation = {
          latnPinyin:"ཕིན་ཡིག"
       }
    },
-   types:{
-      "any":"Any",
-      "corporation":"Corporation",
-      "etext":"འཕྲུལ་ཡིག",
-      "item":"Item", # we don't want to translate this!
-      "lineage":"ཆོས་བརྒྱུད།",
-      "person":"མི་སྣ།",
-      "place":"ས་གནས།",
-      "role":"ལས་འགན།",
-      "topic":"བརྗོད་གཞི།",
-      "work":"བརྩམས་ཆོས།",
-      "instance":"དཔེ་ཆ།"
-   },
    search:{
+      page:"page",
+      result:"Result",
+      result_pl:"Results",
       filters:{
          noresults:"No results matching your filters."
-      }
-   },
-   Lsidebar:{
-      sortBy:{
-         title:"Sort By"
-      },
-      activeF:{
-         title:"Active Filters",
-         remove:"Remove filter",
-         reset:"Clear all filters"
-      },
-      title:"ནང་གསེས་འདེམས་སྒྲུག",
-      collection:{
-         title:"Collection"
-      },
-      datatypes:{
-         title:"དན་གྲངས་ཀྱི་རིགས།"
       }
    },
    Rsidebar:{
@@ -144,15 +300,50 @@ const boTranslation = {
    },
    resource:{
       back:"Go back to search page",
-      browse:"Browse associated resources",
-      collection:"",
-      export:"Export to",
-      in:"",
-      images:"",
+      browse:"Browse associated resources",      
+      openViewer:"Open in Etext Viewer",
+      download:"Download",
+      downloadAs:"Download as",
+      version:"{{format}} version",
+      exportData:"Export {{data}}",
+      exportDataAs:"Export {{data}} as {{format}}",
       settings:"Language Settings",
       toggle:"Toggle annotations panel",
       view:"View",
+      metadata:"metadata",
+      manifest:"IIIF manifest",
+      page:"p.{{num}}",
+      volume:"Vol.{{num}}",
+      availableScans:"available scans for this page",
+      about:"About",
+      createdB:"Created By",
+      extended:"Extended Properties",
+      showD:"Show Details",
+      hideD:"Hide Details",
+      searchO:"Search in outline",
+      start:"Start search",
+      reset:"Reset search"
+
+   },
+   mirador:{
+      return:"return to BUDA",
+      browse:"browse collection",
+      goto:"Go to p.",
+      zoomIn:"zoom in",
+      zoomOut:"zoom out",
+      showI:"Show images",
+      fullscreen:"fullscreen",
+      next:"next image",
+      prev:"previous image",
+      manip:"image manipulation",
+      increaseFont:"Increase Etext font size",
+      decreaseFont:"Decrease Etext font size",
+      downloadE:"Download Etext"
+   },
+   access:{
+      limited20:"Access limited to first & last 20 pages.",
+      credentials:"if you have sufficient credentials to get access to all images from this work."
    }
 } ;
 
-export default boTranslation ;
+export default enTranslation ;
