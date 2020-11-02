6b894d1437823232e75bbb6fc0d757f893eeeda8
berger-n <quai.ledrurollin@gmail.com>
2020-11-02 10:36:41 +0100

    fix testing chinese mirror on library-dev

--- src/lib/api.js
+++ src/lib/api.js
@@ -1019,7 +1019,8 @@ export default class API {
          // to use with ldspdi running locally
          path = "//purl.bdrc.io" +  DICTIONARY_PATH;         
 
-         if(!window.location.href.startsWith("http://localhost") && config && config.chineseMirror) path = "/ldspdi" + DICTIONARY_PATH;
+         if(!window.location.href.startsWith("http://localhost") && !window.location.href.startsWith("http://library-dev.") 
+            && config && config.chineseMirror) path = "/ldspdi" + DICTIONARY_PATH;
 
 
          // to use with ldspdi-dev
