b85335a5a2c531419a7b5239eaed4efe87c93b7c
berger-n <quai.ledrurollin@gmail.com>
2020-12-14 18:22:43 +0100

    use UI language for hasRole/tradition (#403)

--- src/components/App.js
+++ src/components/App.js
@@ -369,7 +369,8 @@ export function highlight(val,k,expand,newline)
 
 const preferUIlang = [ bdo+"placeType", bdo+"workIsAbout", bdo+"workGenre", bdo+"role", bdo+"language", bdo+"script", adm+"metadataLegal", bdo+"personGender", bdo+"printMethod", bdo+"workGenre",
    tmp+"relationType", bdo+"material", adm+"status", adm+"access", adm+"contentLegal", bdo+"partType", adm+"contentLegal",
-   bdo+"binding" ]
+   bdo+"binding",
+   bdo+"tradition", tmp+"hasRole" ]
 
 export function getLangLabel(that:{},prop:string="",labels:[],proplang:boolean=false,uilang:boolean=false,otherLabels:[],dontUseUI:boolean=false)
 {
