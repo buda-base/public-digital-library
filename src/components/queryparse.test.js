46e21d908e08c85abf378fca75d8ff1f7d81389a
eroux <elie.roux@telecom-bretagne.eu>
2021-02-11 10:40:05 +0100

    add test for query syntax

---
+++ src/components/queryparse.test.js
@@ -0,0 +1,19 @@
+require('../setupTests')
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { shallow,mount } from 'enzyme';
+import I18n from 'i18next';
+import store from '../index';
+import {initiateApp} from '../state/actions';
+import {keywordtolucenequery} from "./App"
+
+describe('search bar input to lucene query', () => {
+
+   it('advanced query', async done => {
+      expect(keywordtolucenequery("test AND toto", "en")).toEqual('("test" AND "toto")');
+      expect(keywordtolucenequery("test and toto", "en")).toEqual('"test and toto"');
+      expect(keywordtolucenequery("test and toto", "bo-x-ewts")).toEqual('"test and toto"~1');
+      done()
+   })
+})
