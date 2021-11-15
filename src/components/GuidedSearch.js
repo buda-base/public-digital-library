89b8782e7adebbc444f8abd07b0e6e80cfd24e69
berger-n <quai.ledrurollin@gmail.com>
2021-11-15 18:40:02 +0100

    init guided search sticky frame

--- src/components/GuidedSearch.js
+++ src/components/GuidedSearch.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react"
 import qs from 'query-string'
 import { top_right_menu } from './App'
+import {Link} from "react-router-dom"
 
 type Props = { auth:{}, history:{} }
 type State = { collapse:{} }
@@ -20,13 +21,27 @@ class GuidedSearch extends Component<Props,State> {
       <div>
           <div class={"App home guidedsearch khmer"}>
             <div class="SearchPane">
-                <div className="static-container">
+                <div className="static-container sticky">
                   <div>
                     <h1>Guided Search</h1>                                          
-                    <div id="samples">
-                    </div>
+                    <h2 id="types">Search type</h2>
                   </div>
-                </div> 
+                  <div>
+                    <h2 id="direct">Direct access</h2>
+                    <div style={{ height:"1000px"}}></div>
+                  </div>
+                  <div>
+                    <button class="red">Search</button>
+                  </div>
+                </div>
+                <div>
+                  <nav>
+                    <h3>Navigation</h3>
+                    <Link to="#types">Search type</Link>
+                    <Link to="#direct">Direct access</Link>
+                    <button class="red">Search</button>
+                  </nav>
+                </div>
             </div>
           </div>
           { top_right_menu(this) }
