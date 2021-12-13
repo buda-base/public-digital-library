4818cec21e30541094dad6d6a1caab43cd73e8a8
berger-n <quai.ledrurollin@gmail.com>
2021-12-13 18:06:13 +0100

    hardcoded html/css proof-of-concept for browse page layout

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react"
 import qs from 'query-string'
+import SimpleBar from 'simplebar-react';
+import 'simplebar/dist/simplebar.min.css';
 import { top_right_menu } from './App'
 
 type Props = { auth:{}, history:{}, config:{} }
@@ -24,7 +26,49 @@ class Browse extends Component<Props,State> {
                   <div>
                     <h1>Browse</h1>                                          
                     <div id="samples">
-                    <p style={{"text-align":"center"}}>Coming soon</p>
+                      <div class="head">
+                        <div>param1</div>
+                        <div>param2</div>
+                        <div>param3</div>
+                        <div>title</div>
+                      </div>
+                      <div class="param">
+                        <div class="val on">value1</div>
+                        <div class="param">
+                          <div class="val">value1</div>
+                          <div class="val on">value2</div>
+                          <div class="param">
+                            <div class="val">value1</div>
+                            <div class="val">value2</div>
+                            <div class="val on">value3</div>
+                            <SimpleBar style={{ maxHeight:700 }}>
+                              <div class="param title">
+                                <div class="val">title1</div>
+                                <div class="val">title2</div>
+                                <div class="val">title3</div>
+                                <div class="val">title4</div>
+                                <div class="val">title5</div>
+                                <div class="val">title6</div>
+                                <div class="val">title1</div>
+                                <div class="val">title2</div>
+                                <div class="val">title3</div>
+                                <div class="val">title4</div>
+                                <div class="val">title5</div>
+                                <div class="val">title6</div>
+                                <div class="val">title1</div>
+                                <div class="val">title2</div>
+                                <div class="val">title3</div>
+                                <div class="val">title4</div>
+                                <div class="val">title5</div>
+                                <div class="val">title6</div>
+                              </div>
+                            </SimpleBar>
+                          </div>
+                          <div class="val">value3</div>
+                        </div>
+                        <div class="val">value2</div>
+                        <div class="val">value3</div>
+                      </div>
                     </div>
                   </div>
                 </div> 
