ccce0d39aac99051c270f4b13458b61fe34b042c
berger-n <quai.ledrurollin@gmail.com>
2021-12-14 16:32:47 +0100

    minor bug fix

--- src/state/ui/reducers.js
+++ src/state/ui/reducers.js
@@ -176,19 +176,23 @@ export const browse = (state: UIState, action: actions.Action) => {
     if(!browse) browse = {}
     if(!browse.path) browse.path = []
     if(!browse.path.includes(action.payload)) browse.path.push(action.payload)
-    else if(action.meta?.checked === false) {
-        let j = browse.path.indexOf(action.payload)
+
+    let j = browse.path.indexOf(action.payload)
+    if(action.meta?.checked === false || j < browse.path.length - 1 && j > 0) {
+        
         for(let i = j ; i < browse.path.length ; i ++) {
             if(browse.checked[browse.path[i]]) delete browse.checked[browse.path[i]]
             if(i > 0) delete browse.path[i]
         }
         browse.path = browse.path.filter(p => p)
     }
+
     if(action.meta?.checked !== undefined) {
         if(!browse.checked) browse.checked = {}
         if(action.meta.checked) browse.checked[action.payload] = action.meta.value
         else if(browse.checked[action.payload]) delete browse.checked[action.payload]
     }
+
     return {
         ...state,
         browse
