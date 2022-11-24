fb751ac5644af38a9151b5f8ec734e21a518f7d1
eroux <elie.roux@telecom-bretagne.eu>
2022-11-24 12:13:01 +0100

    fix punctuation regex

--- src/lib/transliterators.js
+++ src/lib/transliterators.js
@@ -91,7 +91,7 @@ export function translitHelper(src,dst) {
 export function ewtsToDisplay(src) {
    src = src.replace(/_/g, " ")
    src = src.replace(/[ \/_*]+$/, "")
-   src = src.replace(/^[ \/_*@#]+$/, "")
+   src = src.replace(/^[ \/_*@#]+/, "")
    return src
 }
 
