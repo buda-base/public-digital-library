b62a6f42cba7df0d5a568b04d19ef40a938530d8
berger-n <quai.ledrurollin@gmail.com>
2023-03-16 15:43:27 +0100

    remove 'find as identifier' when not searching for work or instance (buda-base/lds-queries#38)

--- src/components/App.js
+++ src/components/App.js
@@ -6855,7 +6855,7 @@ handleCheck = (ev:Event,lab:string,val:boolean,params:{}) => {
          }
 
          if(value.match(/(^[0-9]{3,4}$)/) && Number(value) < 2100)  dataSource.unshift(value+"@find as a date")         
-         if(value) dataSource.push(value.replace(/^d([0-9]+)/,"D$1")+"@find as an identifier")
+         if(value && this.state.searchTypes.some(d => ["Instance","Work"].includes(d))) dataSource.push(value.replace(/^d([0-9]+)/,"D$1")+"@find as an identifier")
 
          this.setState({...this.state,keyword:value, language, dataSource,blurSearch:false   } ) 
          
