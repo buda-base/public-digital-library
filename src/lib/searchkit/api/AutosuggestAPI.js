d50e4fb1c2269b851e767b6c79d5cb0490d6d476
berger-n <quai.ledrurollin@gmail.com>
2024-12-20 12:57:34 +0100

    fix autosuggest on tradition pages (#978)

--- src/lib/searchkit/api/AutosuggestAPI.js
+++ src/lib/searchkit/api/AutosuggestAPI.js
@@ -1,23 +1,3 @@
-const getAutocompleteRequest = async (query, filters = "") => {
-  console.log("auto:", query, filters)
-  let scope = filters.split(":")
-  if(scope[0] === "associated_res" && scope[1]) scope = [ scope[1] ]
-  else scope = undefined
-  const url = process.env.REACT_APP_AUTOSUGGEST_HOST;
-  const response = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query, scope }),
-  });
-  return response.json();
-};
-
-export { getAutocompleteRequest };
-
-
-/* // not the problem actually 
 
 const getAutocompleteRequest = async (query, filters = "") => {
   console.log("auto:", query, filters);
@@ -57,5 +37,3 @@ const getAutocompleteRequest = async (query, filters = "") => {
 };
 
 export { getAutocompleteRequest };
-
-*/ 
