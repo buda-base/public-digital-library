15cf1b0a4b77903bf06c7ded162e60b493f0f6c8
berger-n <quai.ledrurollin@gmail.com>
2025-04-18 09:59:03 +0200

    adjust size/color (#1036)

--- src/components/EtextPage.js
+++ src/components/EtextPage.js
@@ -33,7 +33,7 @@ function EtextPage(props) {
 
   const highlight = useCallback((str) => HTMLparse(
     str
-      .replace(/(.rend-small.)/g,"$1 style='vertical-align:"+(0.19+(state_etextSize ?? 1)*0.001)+"em'")
+      .replace(/(.rend-small.)/g,"$1 style='vertical-align:"+(0.12+(state_etextSize ?? 1.5)*0.0075)+"em'")
       .replace(/\[([ ]*)(<[^>]+>)([ ]*)\]/g,"$1$2$3")
       .replace(/[\]\[]*↤[\]\[]*/g,"</span>")
       .replace(/[\]\[]*↦[\]\[]*/g,"<span class='highlight'>")
